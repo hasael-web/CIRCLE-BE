a1773f31240a856dcf2bc8d5eb2e35869f52f471
Andry Pebrianto <andrypeb227@gmail.com>
2023-11-02 11:40:36 +0700

    Create migration file

--- database/data-source.ts
+++ database/data-source.ts
@@ -6,6 +6,7 @@ import { Thread } from "./entities/Thread";
 import { Reply } from "./entities/Reply";
 import { Like } from "./entities/Like";
 import { Upload } from "./entities/Upload";
+import { MigrationFile1698899930953 } from './migration/1698899930953-MigrationFile';
 
 export const PostgreDataSource = new DataSource({
   type: "postgres",
@@ -17,6 +18,6 @@ export const PostgreDataSource = new DataSource({
   synchronize: Env.NODE_ENV === "prod" ? false : true,
   logging: Env.NODE_ENV === "prod" ? false : true,
   entities: [User, Thread, Reply, Like, Upload],
-  migrations: [],
+  migrations: [MigrationFile1698899930953],
   subscribers: [],
 });
