66f259dc686c7533b3cf38f675364184f8691103
Andry Pebrianto <andrypeb227@gmail.com>
2023-11-06 07:14:10 +0700

    Memasanag openapi swagger di route express

--- src/server/createServer.ts
+++ src/server/createServer.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cron from "node-cron";
+import swaggerUi from "swagger-ui-express";
 import handleError from "../utils/exception/handleError";
 import NotFoundError from "../utils/exception/custom/NotFoundError";
 
@@ -11,6 +12,7 @@ import FollowRoutes from "../routes/FollowRoutes";
 import UploadRoutes from "../routes/UploadRoutes";
 import UserRoutes from "../routes/UserRoutes";
 import UploadCron from "../cron/UploadCron";
+import apiSpec from "../utils/swagger/apiSpec";
 
 const createServer: Express = express();
 
@@ -22,13 +24,16 @@ createServer.get("/", (req: Request, res: Response): Response<string> => {
   return res.status(200).send("Server Online!");
 });
 
+createServer.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
+
 createServer.use("/api/v1", AuthRoutes);
 createServer.use("/api/v1", ThreadRoutes);
 createServer.use("/api/v1", FollowRoutes);
 createServer.use("/api/v1", UploadRoutes);
 createServer.use("/api/v1", UserRoutes);
 
-cron.schedule("0 0 * * *", () => { // setiap jam 12 malam
+cron.schedule("0 0 * * *", () => {
+  // setiap jam 12 malam
   UploadCron.cleanUnusedCloudinaryFileDaily();
 });
 
